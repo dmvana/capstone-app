7bb3087b59fa74531b478414ed3af63e43ad7dde
Dawn Vana <dawn.vana@gmail.com>
2016-01-14 17:11:53 -0600

    added flash messages

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,22 +16,35 @@ class QuotesController < ApplicationController
   end
 
   def create
-    @quote. Quote.create(
+    quote. Quote.create(
       description: params[:description],
       date: params[:date],
       user_id: current_user.id 
     )
+    flash[:success] = "Quote was succesfully created!"
     redirect_to '/quotes'
   end
 
   def edit
-    @quote = Quote.find_by(:id params[:id])
+    @quote = Quote.find_by(id: params[:id])
   end
 
   def update
+    quote = Quote.find_by(id: params[:id])
+    quote.update(
+      description: params[:description],
+      date: params[:date],
+      user_id: current_user.id
+    )
+    flash[:success] = "Quote was succesfully updated!"
+    redirect_to "/quotes/#{quote.id}"
   end
 
   def destroy
+    quote = Quote.find_by(id: params[:id])
+    quote.destroy
+    flash[:warning] = "Quote destroyed!"
+    redirect_to '/quotes'
   end
 
 end
